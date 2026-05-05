04343845d4e2c65f91e2365a1e11aed6ff88d318
Sila <aysesilaibis0@gmail.com>
2026-05-05 09:02:04 +0300

    dosyada degisiklikleri yaptim

--- baglan.php
+++ baglan.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "spor_projesi"; // Veritabanı adının doğruluğunu kontrol et
+$db   = "spor_projesi"; // Veritabanı adının doğruluğunu kontrol et Sıla İbiş
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
