8b439077a94c4ab26426f6fec2945e46b0d07dea
yarencalmasurr <yarencalmasurr@mail.com>
2026-05-05 12:16:47 +0300

    güncellendi

--- cikis.php
+++ cikis.php
@@ -1,6 +1,6 @@
 
 <?php
-session_start(); // Mevcut oturumu yakala ŞAMPİYON ERZURUMSPOR
+session_start(); // Mevcut oturumu yakala 
 session_destroy(); // Tüm oturum verilerini (id, rol vb.) sil
 header("Location: index.php"); // Kullanıcıyı giriş sayfasına geri gönder
 exit();
